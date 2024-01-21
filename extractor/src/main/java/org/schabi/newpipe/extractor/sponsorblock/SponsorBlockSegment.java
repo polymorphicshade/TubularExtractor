832dfdaeb71cc6cdd0fe8813c02e033f2293bec2
polymorphicshade <polymorphicshade@gmail.com>
2024-01-20 17:52:53 -0700

    added extractor components for SponsorBlock

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/sponsorblock/SponsorBlockSegment.java
@@ -0,0 +1,29 @@
+package org.schabi.newpipe.extractor.sponsorblock;
+
+import java.io.Serializable;
+
+public class SponsorBlockSegment implements Serializable {
+    public String uuid;
+    public double startTime;
+    public double endTime;
+    public SponsorBlockCategory category;
+    public SponsorBlockAction action;
+
+    public SponsorBlockSegment(final String uuid, final double startTime, final double endTime,
+                               final SponsorBlockCategory category,
+                               final SponsorBlockAction action) {
+        // NOTE: start/end times are in milliseconds
+
+        this.uuid = uuid;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.category = category;
+        this.action = action;
+
+        // since "highlight" segments are marked with the same start time and end time,
+        // increment the end time by 1 second (so it is actually visible on the seekbar)
+        if (this.category == SponsorBlockCategory.HIGHLIGHT) {
+            this.endTime = this.startTime + 1000;
+        }
+    }
+}
